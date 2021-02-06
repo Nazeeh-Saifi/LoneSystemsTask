9963e06f6dfb63b92d0c1200363fa6a76c3eda23
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2021-02-06 10:46:37 +0200

    setting up routes for each controller

--- lOneSystemsApp/routes/api.php
+++ lOneSystemsApp/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SurveyController;
+use App\Http\Controllers\QuestionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResources([
+    'surveys' => SurveyController::class,
+    'questions' => QuestionController::class,
+   
+]);

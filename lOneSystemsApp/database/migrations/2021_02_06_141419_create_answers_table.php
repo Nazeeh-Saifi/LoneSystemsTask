a2e23dfdad3893e082f64be334dbbd6dba32997e
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2021-02-06 17:12:45 +0200

    creating (submission,question) pivot table and named it answers

---
+++ lOneSystemsApp/database/migrations/2021_02_06_141419_create_answers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("SubmissionId");
+            $table->unsignedBigInteger("QuestionId");
+            $table->boolean("answer");
+            $table->timestamps();
+            $table->foreign("SubmissionId")->references("id")->on("submissions")->onDelete("cascade");
+            $table->foreign("QuestionId")->references("id")->on("questions")->onDelete("cascade");
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
